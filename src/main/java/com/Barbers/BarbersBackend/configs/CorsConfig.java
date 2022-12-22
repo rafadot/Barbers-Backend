e61cab06b5d7e13b86cae86597e01a634e12a124
rafadot1 <rafaelairesdemedeiros@outlook.com>
2022-12-22 16:41:25 -0300

    mais uma tentativa

--- src/main/java/com/Barbers/BarbersBackend/configs/CorsConfig.java
+++ src/main/java/com/Barbers/BarbersBackend/configs/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://barbers.herokuapp.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*");
     }
